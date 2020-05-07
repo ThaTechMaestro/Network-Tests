f1dc5cd09eb0c21b8d617743393e96d517007484
AB-Yusuf <engryusufmayowa@gmail.com>
2020-05-07 17:26:49 +0100

    Displays the Operating System of your System

---
+++ ping_test.py
@@ -0,0 +1,17 @@
+import os
+import platform
+import subprocess
+
+
+name_of_platform = platform.system()
+"""
+This returns the name of your System Operating System
+This is stored in "name_of_platform"
+"""
+
+print(name_of_platform)
+"""
+Test your output by running this command
+"""
+
+
