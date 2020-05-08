e69c79e6cdd94d3d36214a09de9cd746f86830ab
AB-Yusuf <engryusufmayowa@gmail.com>
2020-05-08 02:15:59 +0100

    Stores the status code for each process

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# Network-Tests
-Scripts written in Python to test network connectivity
-A step by step explanation of each line of code.
+### Overview
+* Set of Scripts written in Python.
+* These Scripts tests the network connectivity of hosted websites.
+* Tests are carried out with the use of Ping command line utility.
+* These commands are executed as processes.
+* The results of these processes are evaluated to determine if the connection was successful
+* These results are called status codes
+    * 0: Represents Succesfull connection
+    * 1-250+: Represents unsuccessfull conecction
+
+
+
+## Important Technical details 
+For **Linux** OS
+
+A process has three streams for a command to be executed in Linux:
+   * Stdin: Standard input
+   * Stdout: Standard output
+   * Stderr: Standard error
+
+/dev/null : It discards anything written to it
+
+---
+
+## Extra Resources
+[1]. Platform library in python: [Link]https://docs.python.org/2/library/platform.html[Link]
+[2]. Understanding /dev/null: [Link]https://medium.com/@codenameyau/step-by-step-breakdown-of-dev-null-a0f516f53158[Link]
+[3]. Linux standard streams: [Link]https://medium.com/@codenameyau/step-by-step-breakdown-of-dev-null-a0f516f53158[Link]
--- ping_test.py
+++ ping_test.py
@@ -5,13 +5,49 @@ import subprocess
 
 name_of_platform = platform.system()
 """
-This returns the name of your System Operating System
-This is stored in "name_of_platform"
+    This returns the name of your System Operating System
+    This is stored in "name_of_platform"
 """
 
-print(name_of_platform)
-"""
-Test your output by running this command
-"""
+def ping_return_code(name_of_host):
+    """
+        name_of_host -> parameter for hosted websites to be tested
+                        for netwoork connectivity
+    """
+
+    try:
+        """
+            Check for your current system OS
+
+            ping: send an echo request
+            -n: number of request (5)
+            -W: time delay (3) in milliseconds
+            name_of_host: hosted website to be tested for connectivty
+
+            Direct other commands streams such as
+            stdout and stdin to /dev/null (bin)
+
+            Returns only status code for each executed process
+            0: Success
+            1 and above: Error
+        """
+
+        if name_of_platform == "Windows":
+            """
+                For Windows OS
+            """
+            status_code = subprocess.call(['ping', '-n', '5', '-W', '3', name_of_host],
+                                        stdout=open(os.devnull, 'w'),
+                                        stderr=open(os.devnull, 'w'))
+        elif name_of_platform == "Linux":
+            """
+                For Linux OS
+            """
+            status_code = subprocess.call(['ping', '-n', '5', '-W', '3', name_of_host],
+                                        stdout=open(os.devnull, 'w'),
+                                        stderr=open(os.devnull, 'w'))
+
+            
+        
 
 
