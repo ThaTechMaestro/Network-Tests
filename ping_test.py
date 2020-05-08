ba778c90ed89d1e7a89f7e68806e4dbb950c53da
ThaTechMaestro <muse.pearls@gmail.com>
2020-05-08 23:51:18 +0100

    Verifies the connectivity of each host

--- ping_test.py
+++ ping_test.py
@@ -9,7 +9,7 @@ name_of_platform = platform.system()
     This is stored in "name_of_platform"
 """
 
-def ping_return_code(name_of_host):
+def ping_host(name_of_host):
     """
         name_of_host -> parameter for hosted websites to be tested
                         for netwoork connectivity
@@ -46,6 +46,38 @@ def ping_return_code(name_of_host):
             status_code = subprocess.call(['ping', '-n', '5', '-W', '3', name_of_host],
                                         stdout=open(os.devnull, 'w'),
                                         stderr=open(os.devnull, 'w'))
+
+
+        print('Networking Results")
+
+        if status_code == 0:
+            print('Name of host: ', name_of_host)
+            print("Connection result: Sucessful")
+            print('Host is up and running')
+        elif status_code == 1 or 2 or 256 or 512:
+            print('Name of host: ', name_of_host)
+            print("Connection result: Unsucessful")
+            print('Host is down')
+        except Exception:
+            """
+                Other values or unknown errors are captured here and 
+                acted on with the code below
+            """
+            print('Name of host: ', name_of_host)
+            print("Connection result: Unsucessful")
+            print('Host is Unreachable')
+
+        return response
+
+
+def verify_hosts(list_of_hosts):
+    host_status_code = dict()
+
+    for name_of_host in list_of_hosts:
+        host_status_code[name_of_host] = ping_host(name_of_host)
+    return host_status_code
+
+
 
             
         
